2d656c7e0b754b41282230079881249a9fe9f181
Nicholas Perello <nap@Nicholass-MacBook-Pro.local>
2018-12-08 23:01:19 -0500

    adding

---
+++ test.py
@@ -0,0 +1,170 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        #print(x.shape)
+        x = self.pool(F.relu(self.conv1(x)))
+        #print(x.shape)
+        x = self.pool(F.relu(self.conv2(x)))
+        #print(x.shape)
+        x = x.view(-1, 16 * 5 * 5)
+        #print(x.shape)
+        x = F.relu(self.fc1(x))
+        #print(x.shape)
+        x = F.relu(self.fc2(x))
+        #print(x.shape)
+        x = self.fc3(x)
+        #print(x.shape)
+        return x
+
+
+model = Net()
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Convolutional neural network (two convolutional layers)
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=7, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc1 = nn.Sequential(
+            nn.Linear(3*3*384, 512),
+            nn.ReLU())
+        self.fc2 = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU())
+        self.fc3 = nn.Sequential(
+            nn.Linear(512, 10),
+            nn.ReLU())
+
+    def forward(self, x):
+        #print(x.shape)
+        out = self.layer1(x)
+        #print(out.shape)
+        out = self.layer2(out)
+        #print(out.shape)
+        out = self.layer3(out)
+        #print(out.shape)
+        out = F.interpolate(out, size=(3, 3), mode='bilinear')
+        #print(out.shape)
+        out = out.view(out.size(0),-1)
+        #print(out.shape)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return F.log_softmax(out)
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+"""
+torch.Size([4, 3, 32, 32])
+torch.Size([4, 6, 14, 14])
+torch.Size([4, 16, 5, 5])
+torch.Size([4, 400])
+torch.Size([4, 120])
+torch.Size([4, 84])
+torch.Size([4, 10])
+"""
+def check_accuracy(loader, model):
+    if loader.dataset.train:
+        print('Checking accuracy on validation set')
+    else:
+        print('Checking accuracy on test set')
+    num_correct = 0
+    num_samples = 0
+    model.eval()  # set model to evaluation mode
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device, dtype=torch.float)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+            scores = model(x)
+            _, preds = scores.max(1)
+            num_correct += (preds == y).sum()
+            num_samples += preds.size(0)
+        acc = float(num_correct) / num_samples
+        print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc))
+
+# Hyper parameters
+num_epochs = 5
+num_classes = 2
+learning_rate = 0.001
+
+model = ConvNet().to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+total_step = len(trainloader)
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        #print(outputs.shape)
+        #print(labels.shape)
+
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            check_accuracy(testloader, model)
+            running_loss = 0.0
+
+print('Finished Training')
