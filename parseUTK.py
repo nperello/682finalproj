a1a9865ab010a7c30442bacea7cdf449a692fb73
Nicholas Perello <nap@Nicholass-MacBook-Pro.local>
2018-12-09 23:15:27 -0500

    gender by race csv

--- parseUTK.py
+++ parseUTK.py
@@ -1,12 +1,13 @@
 import os, csv
 
-with open("UTKfacesalignrace.csv", 'w') as f:
+with open("UTKfacesaligngenderW.csv", 'w') as f:
     writer = csv.writer(f)
-    writer.writerow(["img_name","race"])#"age","gender","race","date&time"])
+    writer.writerow(["img_name","gender"])#"age","gender","race","date&time"])
     for path, dirs, files in os.walk("alignedimgs"):
         for filename in files:
             labels = filename.split("_")
             if len(labels)==4:
                 print(labels[2])
                 labels[3] = labels[3][:-13]
-                writer.writerow([filename]+[labels[2]])
+                if int(labels[2]) == 0:
+                    writer.writerow([filename]+[labels[1]])
